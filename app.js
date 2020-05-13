a28c9e12278a2dd40959395d63884d37b5517add
mohd-rafey-khan <63442338+mohd-rafey-khan@users.noreply.github.com>
2020-05-13 03:16:30 -0700

    second update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 
+
 // create an app
 const app = express();
 // linked body parser to take data from cliet side..
@@ -16,16 +17,64 @@ app.set("view engine",'ejs');
 // set public folder to interact user more interactive for Singly.
 app.use(express.static('public'));
 
+// connect mongoose
+mongoose.connect("mongodb://localhost/singlyDB",{ useNewUrlParser: true , useUnifiedTopology: true});
+// set shema
+const userschema = new mongoose.Schema({
+  name:"String",
+  email: "String",
+  password: "String"
+});
+
+// create collection Model
+const user =  mongoose.model("user",userschema);
+
+
 // set all route.
-app.get("/home",function(req,res){
-  res.send("its home page..");
-});
+var statusvar = "";
 app.get("/register",function(req,res){
-  res.render("create");
+  res.render("create",{status:statusvar});
 });
+app.post("/register",function(req,res){
+   user.find({ email: req.body.email}, function (err, data) {
+    // console.log(data);
+    if(data.length === 0){
+
+      var newclient = user({
+        name: req.body.name,
+        email:req.body.email,
+        password:req.body.pass
+      });
+      newclient.save();
+      statusvar = "Sucessfully..";
+      res.render("home");
+    }else{
+      console.log(data);
+        statusvar = "Sorry we have already have an Account on this Email..";
+        res.redirect("/register");
+    }
+  });
+
+});
+
 app.get("/",function(req,res){
-  res.render("login");
+  res.render("login",{status:statusvar});
 });
+
+app.post("/",function(req,res){
+
+  user.find({ email: req.body.email}, function (err, data) {
+    console.log(data[0].password);
+     if(data[0].password === req.body.pass){
+       res.render("home");
+     }else{
+       statusvar = "Sorry Please check your Email or Password..";
+       res.redirect("/");
+     }
+  });
+
+});
+// ==============  //
 
 // server listen at 3000 port.
 app.listen(3000,function(req,res){
